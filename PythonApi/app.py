2dc5f5a0f24622b56cd78940513a292d1de9faed
CamilleMMi <camillemichaudmeli@gmail.com>
2023-03-31 10:57:08 +0200

    using black on app.py

--- PythonApi/app.py
+++ PythonApi/app.py
@@ -13,41 +13,49 @@ import secrets
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
-#flash pour les messages
+# flash pour les messages
+
 
 def get_db():
-    return sqlite3.connect('instance/db.sqlite')
+    return sqlite3.connect("instance/db.sqlite")
 
-if not Path('instance/db.sqlite').exists():
+
+if not Path("instance/db.sqlite").exists():
     db = get_db()
-    sql = Path('db.sql').read_text()
+    sql = Path("db.sql").read_text()
     db.executescript(sql)
 
-def checkSession():
-    if 'username' not in session:
-        return None
-    return session['username']
 
-@app.route('/login', methods=['POST', 'GET'])
+def checkSession():
+    if "username" not in session:
+        return None
+    return session["username"]
+
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
     error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'], request.form['password']):
+    if request.method == "POST":
+        if valid_login(request.form["username"], request.form["password"]):
             conn = get_db()
-            result = conn.execute('SELECT id, email FROM users WHERE username = ?', [request.form['username']])
+            result = conn.execute(
+                "SELECT id, email FROM users WHERE username = ?",
+                [request.form["username"]],
+            )
             row = result.fetchone()
-            session['id'] = row[0]
-            session['email'] = row[1]
-            session['username'] = request.form['username']
+            session["id"] = row[0]
+            session["email"] = row[1]
+            session["username"] = request.form["username"]
             conn.close()
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
         else:
-            error = 'Invalid username/password'
-    return render_template('login.html', error=error)
+            error = "Invalid username/password"
+    return render_template("login.html", error=error)
+
 
 def valid_login(username, password):
     conn = get_db()
-    result = conn.execute('SELECT password FROM users WHERE username = ?', [username])
+    result = conn.execute("SELECT password FROM users WHERE username = ?", [username])
     row = result.fetchone()
     conn.close()
     if row is not None and row[0] == password:
@@ -55,27 +63,33 @@ def valid_login(username, password):
     else:
         return False
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
 
-@app.route('/register', methods=['POST', 'GET'])
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["POST", "GET"])
 def register():
     error = None
-    if request.method == 'POST':
-        if register_user(request.form['username'],
-                       request.form['password'],
-                       request.form['email']):
-            return redirect(url_for('index'))
+    if request.method == "POST":
+        if register_user(
+            request.form["username"], request.form["password"], request.form["email"]
+        ):
+            return redirect(url_for("index"))
         else:
-            error = 'Registration failed'
-    return render_template('register.html', error=error)
+            error = "Registration failed"
+    return render_template("register.html", error=error)
+
 
 def register_user(username, password, email):
     conn = get_db()
     try:
-        conn.execute('INSERT INTO users (username, password, email) VALUES (?, ?, ?)', [username, password, email])
+        conn.execute(
+            "INSERT INTO users (username, password, email) VALUES (?, ?, ?)",
+            [username, password, email],
+        )
         conn.commit()
         conn.close()
         return True
@@ -85,13 +99,15 @@ def register_user(username, password, email):
         conn.close()
         return False
 
-@app.route('/')
+
+@app.route("/")
 def index():
     username = checkSession()
     if username is None:
-        return redirect(url_for('login'))
-    return render_template('index.html', username=username)
+        return redirect(url_for("login"))
+    return render_template("index.html", username=username)
 
-@app.route('/addWatchList')
+
+@app.route("/addWatchList")
 def addWatchList():
-    return render_template('addWatchList.html')
+    return render_template("addWatchList.html")
